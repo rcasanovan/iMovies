d1df9b9b35ba58d478043990a3834f2b7dd3bb36
Ricardo Casanova <ricardo@pijp.eu>
2018-09-05 19:53:06 +0200

    Remove medium image url from view model

--- iMovies/Views/Search/View Model/IMMovieViewModel.swift
+++ iMovies/Views/Search/View Model/IMMovieViewModel.swift
@@ -12,17 +12,15 @@ struct IMMovieViewModel {
     
     let id: UInt
     let smallUrlImage: URL?
-    let mediumUrlImage: URL?
     let largeUrlImage: URL?
     let title: String
     let releaseDate: String?
     let overview: String?
     let rating: Float
     
-    init(id: UInt, smallUrlImage: URL?, mediumUrlImage: URL?, largeUrlImage: URL?, title: String, releaseDate: String?, overview: String?, rating: Float) {
+    init(id: UInt, smallUrlImage: URL?, largeUrlImage: URL?, title: String, releaseDate: String?, overview: String?, rating: Float) {
         self.id = id
         self.smallUrlImage = smallUrlImage
-        self.mediumUrlImage = mediumUrlImage
         self.largeUrlImage = largeUrlImage
         self.title = title
         self.releaseDate = releaseDate
@@ -36,11 +34,10 @@ struct IMMovieViewModel {
     
     public static func getViewModelWith(movie: IMSingleMovieResponse) -> IMMovieViewModel {
         let smallUrlImage = IMMovieImageManager.getSmalImageUrlWith(movie.poster_path)
-        let mediumUrlImage = IMMovieImageManager.getMediumImageUrlWith(movie.poster_path)
         let largeUrlImage = IMMovieImageManager.getLargeImageUrlWith(movie.poster_path)
         let releaseDate = Date.getMMMddyyyyFormatWithStringDate(movie.release_date)
         let rating = (movie.vote_average * 5.0) / 10.0
-        return IMMovieViewModel.init(id: movie.id, smallUrlImage: smallUrlImage, mediumUrlImage: mediumUrlImage, largeUrlImage: largeUrlImage, title: movie.title, releaseDate: releaseDate, overview: movie.overview, rating: rating)
+        return IMMovieViewModel.init(id: movie.id, smallUrlImage: smallUrlImage, largeUrlImage: largeUrlImage, title: movie.title, releaseDate: releaseDate, overview: movie.overview, rating: rating)
     }
 
 }
